f83552bafa70d4eb1245667358917f1806fe771c
SqlFR <florian.lazzarini@gmail.com>
2024-08-15 07:05:05 -0400

    merge refacto terminé

--- accounts/views.py
+++ accounts/views.py
@@ -9,25 +9,6 @@ class UserLoginView(LoginView):
     next_page = reverse_lazy('index')
 
 
-# def login_user(request):
-#     next_url = request.GET.get('next', request.POST.get('next', 'index'))
-#
-#     if request.method == 'POST':
-#         # Connecte l'utilisateur
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(username=username, password=password)
-#         if user:
-#             print('User = True', next_url)
-#             login(request, user)
-#             return redirect('index')
-#     else:
-#         next_url = request.GET.get('next', '')
-#         print('User = False', next_url)
-#
-#     return render(request, 'accounts/login.html', {'next': next_url})
-
-
 def logout_user(request):
     logout(request)
     return redirect('login')
